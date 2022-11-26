8e336c74eb07a7e2b822f2105d9648f008971e63
roshankumar04 <90085148+roshankumar04@users.noreply.github.com>
2022-11-26 16:37:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function setup(){
+    canvas=createCanvas(200,200);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+}
+function clearCanvas(){
+    background("white");
+}
+function preload(){
+    classifier=ml5.imageClassifier('Doodlenet');
+}
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas,gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("label").innerHTML='label:'+results[0].label;
+        document.getElementById('confidence').innerHTML='Confidence;'+Math.round(results[0].confidence*100)+'%';
+        utterThis=new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utterThis);
+    }
+}
